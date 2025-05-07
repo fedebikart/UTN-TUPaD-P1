ccfa6573505f7f9d5ddb9a1b79658dc886eb039f
fedebikart <fbikart10@gmail.com>
2025-05-06 22:08:26 -0300

    Trabajo Practico 5 "Listas"

---
+++ 05 Funciones/Trabajo Practico N°5 Listas.py
@@ -0,0 +1,62 @@
+Ejercicio 1 
+lista_multiplos = list(range(4,101,4))
+print (lista_multiplos)
+
+Ejercicio 2
+elementos = ['musica', 'futbol', 'viajar', 'deporte', 'peliculas']
+posicion_3_elementos = elementos [3]
+print (posicion_3_elementos)
+
+Ejercicio 3
+lista_vacia = []
+lista_vacia.append ('programacion')
+lista_vacia.append ('utn')
+lista_vacia.append ('parcial')
+
+print (lista_vacia)
+
+Ejercicio 4
+animales = ["perro", "gato", "conejo", "pez"]
+animales[1] = "loro"
+animales[3] = "oso"
+print (animales)
+
+Ejercicio 5
+El siguiente programa lo que va a realizar es que va a eliminar el numero mas grande que figure en la lista
+
+Ejercicio 6
+lista_valores = list(range(10,31,5))
+posicion_primera = lista_valores[0]
+posicion_segunda = lista_valores[1]
+print (posicion_primera, posicion_segunda)
+
+Ejercicio 7
+autos = ["sedan", "polo", "suran", "gol"]
+autos[1] = ("amarok")
+autos[2] = ("golf")
+print (autos)
+
+Ejercicio 8
+dobles = []
+dobles.append (10)
+dobles.append (20)
+dobles.append (30)
+print (dobles)
+
+Ejercicio 9
+compras = [["pan", "leche"], ["arroz", "fideos", "salsa"],["agua"]]
+
+compras[2].append ("jugo")
+compras[1][1] = "tallarines"
+compras[0].remove ("pan")
+print (compras)
+
+Ejercicio10
+lista_anidada = [[], [], [], []]
+lista_anidada[0].append ("15")
+lista_anidada[1].append  ("True")
+lista_anidada[2].append  ("25.5")
+lista_anidada[2].append  ("57.9")
+lista_anidada[2].append  ("30.6")
+lista_anidada [3].append  ("False")
+print (lista_anidada)
